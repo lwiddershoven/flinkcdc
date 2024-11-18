4f6ff9c4862097c3709874c5710bdb23b07ca938
leon <github@qaz.dds.nl>
2024-11-18 15:10:11 +0100

    Pom with application generated by your home on the 'net: start.spring.io

---
+++ src/main/java/nl/leonw/flinkcdc/FlinkcdcApplication.java
@@ -0,0 +1,13 @@
+package nl.leonw.flinkcdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkcdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlinkcdcApplication.class, args);
+	}
+
+}
