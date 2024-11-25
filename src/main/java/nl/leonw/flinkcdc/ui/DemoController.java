5eb5cfd50a7c6676c71cc87ea9ebb8ac6107f0f4
leon <github@qaz.dds.nl>
2024-11-25 10:53:11 +0100

    Added extra htmx thymeleaf dependency

---
+++ src/main/java/nl/leonw/flinkcdc/ui/DemoController.java
@@ -0,0 +1,11 @@
+package nl.leonw.flinkcdc.ui;
+
+/**
+ * Allows the user to do CRUD operations on the postgres database and
+ * provides insight in what is on the various Kafka topics.
+ * <p>
+ * This is a demo tool.
+ */
+public class DemoController {
+
+}
