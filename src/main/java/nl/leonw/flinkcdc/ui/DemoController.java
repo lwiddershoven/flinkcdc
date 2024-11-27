f5fd61324e063606c1be07ae008f17dee4c33c0d
leon <github@qaz.dds.nl>
2024-11-27 15:38:27 +0100

    Render contents of database using HTMX.

--- src/main/java/nl/leonw/flinkcdc/ui/DemoController.java
+++ src/main/java/nl/leonw/flinkcdc/ui/DemoController.java
@@ -1,11 +1,15 @@
 package nl.leonw.flinkcdc.ui;
 
+import lombok.AllArgsConstructor;
+import nl.leonw.flinkcdc.orders.db.Order;
+import nl.leonw.flinkcdc.orders.db.OrderRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 
 /**
  * Allows the user to do CRUD operations on the postgres database and
@@ -14,9 +18,16 @@ import java.time.LocalDateTime;
  * This is a demo tool.
  */
 @Controller
+@AllArgsConstructor
 public class DemoController {
+
+    private OrderRepository orderRepository;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        var orders = new HashSet<Order>();
+        orderRepository.findAll().forEach(orders::add); // yuck. Mutability in forEach. Also, JPA entity to UI.
+        model.addAttribute("orders", orders);
         return "index";
     }
 
---
+++ src/main/java/nl/leonw/flinkcdc/ui/HtmxErrorController.java
@@ -0,0 +1,28 @@
+package nl.leonw.flinkcdc.ui;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HtmxErrorController {
+
+    private final BasicErrorController basicErrorController;
+
+    public HtmxErrorController(final BasicErrorController basicErrorController) {
+        this.basicErrorController = basicErrorController;
+    }
+
+    @RequestMapping(value = "${server.error.path:${error.path:/error}}", headers = "HX-Request=true")
+    @ResponseStatus(HttpStatus.OK)
+    public ModelAndView errorHtmx(final HttpServletRequest request,
+                                  final HttpServletResponse response) {
+        return basicErrorController.errorHtml(request, response);
+    }
+
+}
