0bc9bb2645346b6ef937acb7c7cc589b7c727c24
leon <github@qaz.dds.nl>
2024-11-28 11:24:25 +0100

    Update thymeleaf import syntax. Add start of inline edit functionality.

--- src/main/java/nl/leonw/flinkcdc/ui/DemoController.java
+++ src/main/java/nl/leonw/flinkcdc/ui/DemoController.java
@@ -6,10 +6,12 @@ import nl.leonw.flinkcdc.orders.db.OrderRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
+import java.util.UUID;
 
 /**
  * Allows the user to do CRUD operations on the postgres database and
@@ -29,6 +31,19 @@ public class DemoController {
         orderRepository.findAll().forEach(orders::add); // yuck. Mutability in forEach. Also, JPA entity to UI.
         model.addAttribute("orders", orders);
         return "index";
+    }
+
+    @GetMapping("/orders/{order-id}/edit")
+    public String editOrder(Model model, @PathVariable("order-id") UUID orderId) {
+        orderRepository.findById(orderId);
+        model.addAttribute("now", LocalDateTime.now().toString());
+        return "/fragments/dbitems :: order-row";
+    }
+
+    @GetMapping("/orders/{order-id}/items/{item-id}/edit")
+    public String editItem(Model model, @PathVariable("order-id") UUID orderId, @PathVariable("item-id") UUID itemId) {
+        model.addAttribute("now", LocalDateTime.now().toString());
+        return "/fragments/dbitems :: item-row";
     }
 
     @PostMapping("/clicked")
