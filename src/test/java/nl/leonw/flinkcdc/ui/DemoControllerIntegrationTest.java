72c16c950c1c1ba105327fedfb3adcb3c397af7b
leon <github@qaz.dds.nl>
2024-12-10 13:42:34 +0100

    Replaced postgres wait strategy for one that works for me.

--- src/test/java/nl/leonw/flinkcdc/ui/DemoControllerIntegrationTest.java
+++ src/test/java/nl/leonw/flinkcdc/ui/DemoControllerIntegrationTest.java
@@ -2,23 +2,20 @@ package nl.leonw.flinkcdc.ui;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import javax.sql.DataSource;
 import java.util.UUID;
 
-import static java.lang.Thread.sleep;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -31,34 +28,18 @@ class DemoControllerIntegrationTest {
     static final UUID ORDER_ID = UUID.fromString("2cf5181c-5185-4da1-ba2c-f0d8f334679c");
 
     @Container
+    @ServiceConnection
     private static final PostgreSQLContainer<?> POSTGRES_CONTAINER =
             new PostgreSQLContainer<>("postgres:17.2-alpine") // same as docker-compose
                     .withDatabaseName("testdb")
                     .withUsername("testuser")
-                    .withPassword("testpass");
+                    .withPassword("testpass")
+                    // Default:  this.waitStrategy = (new LogMessageWaitStrategy()).withRegEx(".*database system is ready to accept connections.*\\s").withTimes(2).withStartupTimeout(Duration.of(60L, ChronoUnit.SECONDS));
+                    // but that triggers before the listening port is available :(
+                    .waitingFor(Wait.forListeningPorts());
 
     @LocalServerPort
     private int port;
-
-    @TestConfiguration
-    static class TestConfig {
-        @Bean
-        public DataSource dataSource() {
-            try {
-                // While postgres is running it's not yet really really ready - maybe the port forward is not set up?
-                // TODO poll in a more robust way, probably the log message followed by host port wait strategy. No prio though
-                sleep(1000L);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
-            return DataSourceBuilder.create()
-                    .url(POSTGRES_CONTAINER.getJdbcUrl())
-                    .username(POSTGRES_CONTAINER.getUsername())
-                    .password(POSTGRES_CONTAINER.getPassword())
-                    .build();
-        }
-    }
 
     @Test
     @DisplayName("Cannot GET an individual order")
