0ab08afe761dab9f225d814f4c3da52d38b87cb1
leon <github@qaz.dds.nl>
2024-12-17 10:06:15 +0100

    Add ArchUnit to guarantee the stream processing stuff is independent. I'd like to put it in a separate service but that is quite a lot of overhead in repo, projects, deployments etc. So we just combine it in 1 deployable but separate it through archunit-verified isolation.

---
+++ src/test/java/nl/leonw/flinkcdc/ui/ArchitectureTest.java
@@ -0,0 +1,14 @@
+package nl.leonw.flinkcdc.ui;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ArchitectureTest {
+
+    @Test
+    @DisplayName("Ensure streamtransform packages are independent of the main app")
+    void test() {
+
+    }
+}
