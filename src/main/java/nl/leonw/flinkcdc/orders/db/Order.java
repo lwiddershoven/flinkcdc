88579b96121dec5e5fdc2c1911bacae45d97bd55
leon <github@qaz.dds.nl>
2025-02-07 18:52:41 +0100

    Domain: Remove lombok. Lombok with Java 21 and intelliJ is just a bit too much work.

--- src/main/java/nl/leonw/flinkcdc/orders/db/Order.java
+++ src/main/java/nl/leonw/flinkcdc/orders/db/Order.java
@@ -7,14 +7,12 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.Data;
 import org.hibernate.annotations.UuidGenerator;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Set;
 import java.util.UUID;
 
-@Data
 @Entity
 @Table(name = "orders")
 @EntityListeners(AuditingEntityListener.class)
@@ -34,4 +32,60 @@ public class Order {
     private Set<OrderItem> items;
 
     private AuditMetadata auditMetaData = new AuditMetadata();
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getDeliveryAddressId() {
+        return deliveryAddressId;
+    }
+
+    public void setDeliveryAddressId(String deliveryAddressId) {
+        this.deliveryAddressId = deliveryAddressId;
+    }
+
+    public long getTotalPriceExVatCents() {
+        return totalPriceExVatCents;
+    }
+
+    public void setTotalPriceExVatCents(long totalPriceExVatCents) {
+        this.totalPriceExVatCents = totalPriceExVatCents;
+    }
+
+    public long getTotalVatCents() {
+        return totalVatCents;
+    }
+
+    public void setTotalVatCents(long totalVatCents) {
+        this.totalVatCents = totalVatCents;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(Set<OrderItem> items) {
+        this.items = items;
+    }
+
+    public AuditMetadata getAuditMetaData() {
+        return auditMetaData;
+    }
+
+    public void setAuditMetaData(AuditMetadata auditMetaData) {
+        this.auditMetaData = auditMetaData;
+    }
 }
--- src/main/java/nl/leonw/flinkcdc/orders/db/OrderItem.java
+++ src/main/java/nl/leonw/flinkcdc/orders/db/OrderItem.java
@@ -4,13 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
 import org.hibernate.annotations.UuidGenerator;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.UUID;
 
-@Data
 @Entity
 @Table(name = "items")
 @EntityListeners(AuditingEntityListener.class)
@@ -27,4 +25,52 @@ public class OrderItem {
     private long vatPerItemCents;
 
     private AuditMetadata auditMetaData = new AuditMetadata();
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public long getPricePerItemExVatCents() {
+        return pricePerItemExVatCents;
+    }
+
+    public void setPricePerItemExVatCents(long pricePerItemExVatCents) {
+        this.pricePerItemExVatCents = pricePerItemExVatCents;
+    }
+
+    public long getVatPerItemCents() {
+        return vatPerItemCents;
+    }
+
+    public void setVatPerItemCents(long vatPerItemCents) {
+        this.vatPerItemCents = vatPerItemCents;
+    }
+
+    public AuditMetadata getAuditMetaData() {
+        return auditMetaData;
+    }
+
+    public void setAuditMetaData(AuditMetadata auditMetaData) {
+        this.auditMetaData = auditMetaData;
+    }
 }
