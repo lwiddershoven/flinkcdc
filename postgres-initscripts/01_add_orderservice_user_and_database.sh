6a584245730a9f2d76f11ab2f68c1d8712944e8e
leon <github@qaz.dds.nl>
2025-02-09 15:00:44 +0100

    Postgres: Moved orders from public to orderservice schema

--- postgres-initscripts/01_add_orderservice_user_and_database.sh
+++ postgres-initscripts/01_add_orderservice_user_and_database.sh
@@ -9,7 +9,9 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 EOSQL
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "orders" <<-EOSQL
-  GRANT ALL PRIVILEGES ON SCHEMA public TO orderservice_user;
-  GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO orderservice_user;
-  GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO orderservice_user;
+  CREATE SCHEMA orderservice;
+  ALTER USER orderservice_user SET search_path TO orderservice;
+  GRANT ALL PRIVILEGES ON SCHEMA orderservice TO orderservice_user;
+  GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA orderservice TO orderservice_user;
+  GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA orderservice TO orderservice_user;
 EOSQL
--- postgres-initscripts/02_add_debezium_user.sh
+++ postgres-initscripts/02_add_debezium_user.sh
@@ -5,8 +5,10 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "orders" <<-EOSQL
   CREATE PUBLICATION connect_order_publication FOR ALL TABLES; -- Debezium is not superuser so can't do this for us.
   CREATE ROLE debezium_user WITH REPLICATION LOGIN PASSWORD 'debezium_password';
   GRANT CONNECT ON DATABASE orders TO debezium_user;
-  GRANT USAGE ON SCHEMA public TO debezium_user;
-  GRANT SELECT ON ALL TABLES IN SCHEMA public TO debezium_user; -- tables already present
-  ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO debezium_user; -- tables created in the future
+
+  GRANT USAGE ON SCHEMA orderservice TO debezium_user;
+
+  GRANT SELECT ON ALL TABLES IN SCHEMA orderservice TO debezium_user; -- tables already present
+  ALTER DEFAULT PRIVILEGES IN SCHEMA orderservice GRANT SELECT ON TABLES TO debezium_user; -- tables created in the future
   GRANT orderservice_user TO debezium_user; -- debezium_user has to be superuser or co-owner of tables
 EOSQL
